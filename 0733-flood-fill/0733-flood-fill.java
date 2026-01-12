c94d2894c5a01af1b44a426da58bd8d6c296baac
Bhumi Singh <singhbhumi0927@gmail.com>
2026-01-12 21:50:55 +0530

    Time: 0 ms (100%), Space: 47 MB (29.23%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][]=new boolean[image.length][image[0].length];
+        int orgCol = image[sr][sc];
+
+        if (orgCol == color) return image;
+        helper(image,sr,sc,color,vis,orgCol);
+        return image;
+    }
+    public void helper(int[][] image,int sr,int sc,int color,boolean vis[][],int orgCol){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || vis[sr][sc] || image[sr][sc]!=orgCol ){
+            return;
+        }
+        vis[sr][sc] = true;
+        image[sr][sc] = color;
+        helper(image,sr,sc-1,color,vis,orgCol);
+        helper(image,sr,sc+1,color,vis,orgCol);
+        helper(image,sr-1,sc,color,vis,orgCol);
+        helper(image,sr+1,sc,color,vis,orgCol);
+    }
+}
