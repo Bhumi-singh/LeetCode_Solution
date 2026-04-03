0580448b26395c0e34699e3bb1b1c540db239bf4
Bhumi Singh <singhbhumi0927@gmail.com>
2026-04-03 14:04:06 +0530

    Time: 986 ms (41.73%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -4,7 +4,8 @@ WITH new_table AS (
         d.name AS Department,
         e.name AS Employee,
         e.salary AS Salary,
-        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranking
+        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) 
+        AS ranking
     FROM Employee e
     LEFT JOIN Department d
     ON e.departmentId = d.id
