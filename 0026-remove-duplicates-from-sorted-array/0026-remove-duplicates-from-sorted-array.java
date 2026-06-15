307f99e126e6a8660a13712790c8cd0f677a3825
Bhumi Singh <singhbhumi0927@gmail.com>
2026-06-15 10:30:39 +0530

    Time: 0 ms (100%), Space: 46.7 MB (61.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        ArrayList<Integer> temp=new ArrayList<>();
-        temp.add(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]) temp.add(nums[i]);
-        }
-
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp.get(i);
-        }
-        return temp.size();
-        // int i=0;
-        // for(int j=1;j<nums.length;j++){
-        //     if(nums[j]!=nums[i]){
-        //         nums[i+1]=nums[j];
-        //         i++;
-        //     }
+        // ArrayList<Integer> temp=new ArrayList<>();
+        // temp.add(nums[0]);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]!=nums[i-1]) temp.add(nums[i]);
         // }
-        // return (i+1);
+
+        // for(int i=0;i<temp.size();i++){
+        //     nums[i]=temp.get(i);
+        // }
+        // return temp.size();
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return (i+1);
     }
 }
