e2c585fc6f8ce816aa2c5853b8f7f37c9d5a133b
Bhumi Singh <singhbhumi0927@gmail.com>
2026-06-23 14:03:50 +0530

    Time: 220 ms (14.11%), Space: 111.6 MB (36.7%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,34 +1,26 @@
 class MedianFinder {
 
-    ArrayList<Integer> list;
-
+    PriorityQueue<Integer> left;//max heap
+    PriorityQueue<Integer> right;
     public MedianFinder() {
-        list=new ArrayList<>();
+        left=new PriorityQueue<>(Collections.reverseOrder());
+        right=new PriorityQueue<>();
     }
     
     public void addNum(int num) {
-        int idx=findpos(list,num);
-        list.add(idx,num);
-    }
-    private int findpos(ArrayList<Integer> list,int num){
-        int left=0;
-        int right=list.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(list.get(mid)<num){
-                left=mid+1;
-            }else{
-                right=mid-1;
-            }
+        left.add(num);
+        right.add(left.remove());
+
+        while(right.size()>left.size()){
+            left.add(right.remove());
         }
-        return left;
     }
     
     public double findMedian() {
-        
-        int n=list.size();
-        if(n%2==1) return list.get(n/2);
-        return (list.get(n/2)+list.get(n/2-1))/2.0;
+        if(left.size()>right.size()){
+            return left.peek();
+        }
+        return (left.peek()+right.peek())/2.0;
     }
 }
 
@@ -38,3 +30,38 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+//  class MedianFinder {
+
+//     ArrayList<Integer> list;
+
+//     public MedianFinder() {
+//         list=new ArrayList<>();
+//     }
+    
+//     public void addNum(int num) {
+//         int idx=findpos(list,num);
+//         list.add(idx,num);
+//     }
+//     private int findpos(ArrayList<Integer> list,int num){
+//         int left=0;
+//         int right=list.size()-1;
+//         while(left<=right){
+//             int mid=left+(right-left)/2;
+//             if(list.get(mid)<num){
+//                 left=mid+1;
+//             }else{
+//                 right=mid-1;
+//             }
+//         }
+//         return left;
+//     }
+    
+//     public double findMedian() {
+        
+//         int n=list.size();
+//         if(n%2==1) return list.get(n/2);
+//         return (list.get(n/2)+list.get(n/2-1))/2.0;
+//     }
+// }
+
