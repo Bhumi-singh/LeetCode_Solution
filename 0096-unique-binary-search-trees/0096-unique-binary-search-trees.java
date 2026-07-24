67916381494edab33e2e3d3facba25e0fe782f9d
Bhumi Singh <singhbhumi0927@gmail.com>
2026-07-24 11:35:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (84.52%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numTrees(int n) {
-        long ans = 1;
-
-        for (int i = 0; i < n; i++) {
-            ans = ans * (2L * n - i) / (i + 1);
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int nodes=2;nodes<=n;nodes++){
+            for(int root=1;root<=nodes;root++){
+                dp[nodes]+=dp[root-1]*dp[nodes-root];
+            }
         }
-
-        ans /= (n + 1);
-
-        return (int) ans;
+        return dp[n];
     }
 }
