485577e6c399f7e0ad8799a9020752f6af4ccdfe
Bhumi Singh <singhbhumi0927@gmail.com>
2026-09-16 23:15:36 +0530

    Time: 16 ms (93.35%), Space: 45.7 MB (77.33%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n=nums.length;
-        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                    long sum=(long) nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum<target) k++;
-                    else if(sum>target) l--;
+                int l=j+1;
+                int r=n-1;
+                while(l<r){
+                    long sum=(long)nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum<target) l++;
+                    else if(sum>target) r--;
                     else{
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        k++;
-                        l--;
-                        while(k<l && nums[k]==nums[k-1]) k++;
-                        while(k<l && nums[l]==nums[l+1]) l--;
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                        l++;r--;
+                        while(l<r && nums[l]==nums[l-1]) l++;
+                        while(l<r && nums[r]==nums[r+1]) r--;
                     }
                 }
             }
